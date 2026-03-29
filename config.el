d5cf507f33f398e8deedc4bcda0a7308ff681841
Lu Zhu <misterzhu@gmail.com>
2026-03-29 13:31:03 -0700

    Add help binding

--- config.el
+++ config.el
@@ -8,6 +8,7 @@
 (global-set-key "\M-n" 'highlight-symbol-next)
 (global-set-key "\M-p" 'highlight-symbol-prev)
 (global-set-key [?\C-h] 'delete-backward-char)
+(global-set-key "\C-ch" 'help-command)
 (global-set-key "\M-0" 'treemacs-select-window)
 (setq confirm-kill-emacs nil)
 
