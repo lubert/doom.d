1d3008e70477a05696ba33df324b67e1df8ad3c3
Lu Zhu <misterzhu@gmail.com>
2026-03-31 15:13:09 -0700

    Better contrast

--- config.el
+++ config.el
@@ -16,15 +16,20 @@
 (add-hook! 'flycheck-mode-hook #'flycheck-set-indication-mode)
 
 (setq doom-theme 'doom-zenburn)
+(custom-set-faces!
+  '(hl-line :background "#2B2B2B")
+  '(vertico-current :background "#4A4A4A"))
 (setq display-line-numbers-type t)
 (setq org-directory "~/org/")
 
+(set-popup-rule! "^\\*eldoc" :select t :side 'bottom :size 0.3)
+
 (after! eglot
-  (setq eglot-events-buffer-size 0)  ; disable event logging for performance
-  (setq eglot-autoshutdown t)        ; shut down server when last buffer is killed
-  (setq eglot-connect-timeout 10)    ; don't block forever waiting for server
-  (setq eglot-sync-connect nil)      ; connect asynchronously, don't block UI
-  (fset #'eglot--message #'ignore))  ; suppress "connected" and other eglot messages
+  (setq eglot-events-buffer-size 0)
+  (setq eglot-autoshutdown t)
+  (setq eglot-connect-timeout 10)
+  (setq eglot-sync-connect nil)
+  (fset #'eglot--message #'ignore))
 
 (add-hook 'find-file-hook #'diff-hl-margin-mode)
 (add-hook 'find-file-hook #'diff-hl-mode)
